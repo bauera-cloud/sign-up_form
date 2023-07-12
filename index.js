94f13ba97d79626565c717ada77a44be7a058a9a
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-07-12 18:14:12 -0400

    Add favicon, add custom error message and icon

---
+++ index.js
@@ -0,0 +1,8 @@
+let emailInput = document.querySelector('#email')
+let emailErrorMessage = document.querySelector('#email-error-message');
+
+
+// emailInput.addEventListener('change', (e) => {
+//     emailErrorMessage.hidden = false;
+//     //remove hidden attribute
+// })
