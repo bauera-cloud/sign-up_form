cab046619e3d7faed888bd4ea53088787fb78039
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-07-13 10:22:34 -0400

    finish styling, add custom error message
    There was too much padding which caused the page to be too wide.
    
    Secondly, the image did not cover the page which caused a scroll bar
    to appear vertically. The image would shrink but now it's retaining
    the height of the page when the width of the page changes.
    
    input:focus border was changed but doesn't apply to the button. Added
    another custom error for the phone number using the regex pattern in js.
    
    Lastly, the odin logo, credit, and semi-transparent background was in an
    absolute fixed position. Now, its position is relative to the background
    image.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let emailInput = document.querySelector('#email')
-let emailErrorMessage = document.querySelector('#email-error-message');
+let firstName = document.querySelector('#first-name');
+let firstNameErrorMessage = document.querySelector('#name-error-message');
 
+let phoneNumber = document.querySelector('#phone-number')
+let phoneNumberErrorMessage = document.querySelector('#phone-number-error-message');
 
-// emailInput.addEventListener('change', (e) => {
-//     emailErrorMessage.hidden = false;
-//     //remove hidden attribute
-// })
+//reveals error message
+firstName.addEventListener('change', (e) => {
+    console.log(e.target.value)
+    if (/[a - zA - Z]+/.test(e.target.value) && e.target.value.length < 2) {
+        firstNameErrorMessage.style.visibility = 'visible';
+    }
+})
+
+phoneNumber.addEventListener('change', (e) => {
+    if (!/\d{3}[\-]\d{3}[\-]\d{4}/.test(e.target.value)) {
+        phoneNumberErrorMessage.style.visibility = 'visible';
+    }
+})
