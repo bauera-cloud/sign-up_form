30a2c860a70df07e9012f3f852087bf8d4f114ae
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-07-13 13:04:50 -0400

    Add more custom error validation messages

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let firstName = document.querySelector("input[id='first-name'");
 let firstNameErrorMessage = document.querySelector('#name-error-message');
 
+let lastName = document.querySelector("input[id='last-name'");
+let lastNameErrorMessage = document.querySelector('#last-name-error-message');
+
 let phoneNumber = document.querySelector("input[id='phone-number'");
 let phoneNumberErrorMessage = document.querySelector('#phone-number-error-message');
 
+let password = document.querySelector("input[id='password'");
+let passwordErrorMessage = document.querySelector('#password-error-message');
+
 //reveals error message
 firstName.addEventListener('blur', (e) => {
-    console.log(e.target.value)
     if (/[a - zA - Z]+/.test(e.target.value) && e.target.value.length < 2) {
         firstNameErrorMessage.style.visibility = 'visible';
     }
@@ -15,6 +20,18 @@ firstName.addEventListener('blur', (e) => {
 firstName.addEventListener('focus', (e) => {
     if (firstNameErrorMessage.checkVisibility() === true) {
         firstNameErrorMessage.style.visibility = 'hidden';
+    }
+})
+
+lastName.addEventListener('blur', (e) => {
+    if (/[a - zA - Z]+/.test(e.target.value) && e.target.value.length < 2) {
+        lastNameErrorMessage.style.visibility = 'visible';
+    }
+})
+
+lastName.addEventListener('focus', (e) => {
+    if (lastNameErrorMessage.checkVisibility() === true) {
+        lastNameErrorMessage.style.visibility = 'hidden';
     }
 })
 
@@ -30,3 +47,19 @@ phoneNumber.addEventListener('focus', (e) => {
         phoneNumberErrorMessage.style.visibility = 'hidden';
     }
 })
+
+//error in console: caught SyntaxError: Invalid regular expression: Invalid Group
+// password.addEventListener('blur', (e) => {
+//     if (!/(?=^.{ 8, }$)((?=.*\d) | (?=.*\W +)) (? ![.\n])(?=.* [A - Z])(?=.* [a - z]).*$/.test(e.target.value)) {
+//         passwordErrorMessage.style.visibility = 'visible';
+//     }
+// })
+
+password.addEventListener('focus', (e) => {
+    if (passwordErrorMessage.checkVisibility() === true) {
+        passwordErrorMessage.style.visibility = 'hidden';
+    }
+})
+
+//error for confirm password needs to be added to this file, html, and css.
+
